63c3d797b3ba22c915c5826967fb373a5a5aadf6
jetnet <jetnet@github.com>
2026-02-18 22:06:31 +0100

    chore: add eslint dependencies and config for standalone linting
    Add eslint, @typescript-eslint plugins to devDependencies.
    Add .eslintrc.js config for standalone linting without OSD context.

---
+++ opensearch_index_manager/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+  },
+  rules: {
+    // Disable rules that require OSD types
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+  ignorePatterns: [
+    'build/',
+    'node_modules/',
+    '*.js',
+    '*.d.ts',
+  ],
+};
